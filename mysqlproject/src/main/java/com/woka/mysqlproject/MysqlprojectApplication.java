6698d73af555c074ab73d72d9277ee256a9271ab
Woka Aditama <woka@ptmitrabhakti.com>
2024-06-10 05:22:38 +0700

    developed mysql project nolimit

---
+++ mysqlproject/src/main/java/com/woka/mysqlproject/MysqlprojectApplication.java
@@ -0,0 +1,13 @@
+package com.woka.mysqlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlprojectApplication.class, args);
+	}
+
+}
---
+++ mysqlproject/src/test/java/com/woka/mysqlproject/MysqlprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woka.mysqlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
